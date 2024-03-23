4e0fa14709cdece5a771a25b5242cc42aa4f5336
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-03-23 10:23:17 -0400

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const conn = require("./db/conn");
+
+conn.authenticate().then(()=> {
+    console.log("Conectado com sucesso!");
+}).catch((err)=>{
+    console.log("Ocorreu um erro: " + err)
+});
