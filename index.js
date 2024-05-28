24bf6f0052630f6a1b9b7b30d463c16290a04b48
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-05-28 17:42:17 -0400

    commit

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
+// Importaçõoes dos módulos
 require("dotenv").config();
 const conn = require("./db/conn");
-const Usuario = require("./models/Usuarios");
 const express = require("express");
-const exphbs = require("express");
+const exphbs = require("express-handlebars");
 
+// Instanciação do servidor
 const app = express();
 
+// Vinculação do Handlebars ao Express
 app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars")
+app.set("view engine", "handlebars");
 
+// Configurações no express para facilitar a captura de dados recebidos de formulários
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-app.use(espress.json());
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.use(express.json());
+
+
+const Usuario = require("./models/Usuario");
 
 app.get("/usuarios/novo", (req, res) => {
-    res.render("formUsuario");
-});
-
-app.post("/usuarios/novo", (req, res) =>{
-    const nickname = req.body.nickname;
-    const  nome = req.body.nome;
-     
-    const dadosUsuario = {
-        nickname,
-        nome,
-    };
-
-    const usuario = Usuario.create(dadosUsuario);
-
-    res.send("Usuário inserido sob o id "+ usuario.id);
-});
-
-app.listen(8000);
-
-conn.
-sync()
-.then(()=> {
-    console.log("Conectado com sucesso!");
+  res.render("formUsuario")
 })
-.catch((err)=>{
-    console.log("Ocorreu um erro: " + err)
+
+const usuarios = db.define
+
+app.post("/usuarios/novo", async (req, res) => {
+  const nickname = req.body.nickname;
+  const nome = req.body.nome;
+
+  const dadosUsuario = {
+    nickname,
+    nome,
+  };
+
+  const usuario = await Usuario.create(dadosUsuario);
+  res.send("Usuário inserido: " + usuario.id)
 });
+
+app.listen(3000);
+
+conn
+  .sync()
+  .then(() => {
+    console.log("Conectado com sucesso :)");
+  })
+  .catch((err) => {
+    console.log("Erro ao conectar: " + err);
+  });
